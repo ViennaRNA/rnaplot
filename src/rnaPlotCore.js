a8218e2b60e0121898097f19ec55032cedf0b987
Peter Kerpedjiev <pkerp@tbi.univie.ac.at>
2015-05-27 14:50:26 +0200

    Refactoring, added labels and padding

--- src/rnaPlotCore.js
+++ src/rnaPlotCore.js
@@ -6,99 +6,161 @@ function rnaPlot() {
     transitionLength = 0,
     uids = [];
 
-    function addNucleotideNames(selection, rnaGraph) {
-        // the labels for each nucleotide
+    var options = {
+        "origNucleotideRadius": 5,
+        "rnaEdgePadding": 3
+    };
+
+    function createTransformToFillViewport(xValues, yValues) {
+        // create transform that will scale the x and y values so that
+        // they fill the available viewport
+    
+        // find out leftmost, rightmost, topmost, bottommost positions of each
+        // nucleotide so that we can create a scale
+        var xExtent = d3.extent(rg.nodes.map(function(d) { return d.x; })) 
+        var yExtent = d3.extent(rg.nodes.map(function(d) { return d.y; })) 
+
+        console.log('xs:', rg.nodes.map(function(d) { return d.x; }));
+
+        // add the radius of the nucleotides
+        var origNucleotideRadius = 5;
+        xExtent[0] -= origNucleotideRadius + options.rnaEdgePadding;
+        yExtent[0] -= origNucleotideRadius + options.rnaEdgePadding;
+
+        xExtent[1] += origNucleotideRadius + options.rnaEdgePadding;
+        yExtent[1] += origNucleotideRadius + options.rnaEdgePadding;
+
+        // find out how wide and height the molecule
+        var xRange = xExtent[1] - xExtent[0];
+        var yRange = yExtent[1] - yExtent[0];
+
+        // how much wider / taller is it than the available viewport
+        var xExtra = xRange - width;
+        var yExtra = yRange - height;
+
+        var xScale, yScale;
+
+        // once we have a scale for one dimension, we can create the scale for the other
+        // keeping the same expansion / shrinking ratio
+        function createOtherScale(firstScale, newDomain, newRange) {
+            var scaleFactor = (firstScale.range()[1] - firstScale.range()[0]) / 
+                              (firstScale.domain()[1] - firstScale.domain()[0]);
+            var newWidth = (newDomain[1] - newDomain[0]) * scaleFactor
+            var newMargin = ((newRange[1] - newRange[0]) - newWidth) / 2;
+
+            console.log('scaleFactor', scaleFactor);
+
+            return {"scaleFactor": scaleFactor, 
+                    "scale": d3.scale.linear()
+                                     .domain(newDomain)
+                                     .range([newRange[0] + newMargin, newRange[1] - newMargin])};
+        }
+
+        var ret;
+
+        console.log('xRange:', xExtent, 'yRange:', yExtent);
+        console.log('xExtra:', xExtra, 'yExtra:', yExtra);
+
+        if (xExtra > yExtra) {
+            // we have to shrink more in the x-dimension than the y
+            xScale = d3.scale.linear()
+            .domain(xExtent)
+            .range([0, width])
+
+            ret = createOtherScale(xScale, yExtent, [0, height]);
+            yScale = ret.scale;
+        } else {
+            // we have to shrink more in the x-dimension than the y
+            yScale = d3.scale.linear()
+            .domain(yExtent)
+            .range([0, height])
+
+            ret = createOtherScale(yScale, xExtent, [0, width]);
+            xScale = ret.scale;
+        }
+
+        var xOffset = xScale.range()[0] - xScale.domain()[0];
+        var yOffset = yScale.range()[0] - yScale.domain()[0];
+
+        return "translate(" + -(xScale.domain()[0] * ret.scaleFactor - xScale.range()[0]) + 
+                  "," + -(yScale.domain()[0] * ret.scaleFactor - yScale.range()[0]) + ")" + 
+            'scale(' + ret.scaleFactor + ')';
+    }
+
+    function createNucleotides(selection, nucleotideNodes) {
+        // create groupings for each nucleotide and label
+        var gs = selection
+        .selectAll('.rnaBase')
+        .data(nucleotideNodes)
+        .enter()
+        .append('svg:g')
+        .attr('transform', function(d) { 
+            return 'translate(' + d.x + ',' + d.y + ')'; 
+        });
+
+        var circles = gs.append('svg:circle')
+        .attr('r', options.origNucleotideRadius)
+        .classed('rnaBase', true)
+    }
+
+    function createLabels(selection, labelNodes) {
+        // create groupings for each nucleotide and label
+
+        console.log('labelNodes', labelNodes)
+
+        var gs = selection 
+        .selectAll('.rnaLabel')
+        .data(labelNodes)
+        .enter()
+        .append('svg:g')
+        .attr('transform', function(d) { 
+            return 'translate(' + d.x + ',' + d.y + ')'; 
+        });
+
+        var circles = gs.append('svg:circle')
+        .attr('r', options.origNucleotideRadius)
+        .classed('rnaBase', true)
+        .classed('rnaLabel', true)
+
     }
 
     function chart(selection) {
         console.log('selection', selection)
         selection.each(function(data) {
-            console.log('data', data)
             // data should be a dictionary containing at least a structure
             // and possibly a sequence
+            console.log('data', data)
             rg = new RNAGraph(data.sequence, data.structure, data.name)
                     .recalculateElements()
                     .elementsToJson();
-            var positions;
-            var origRadius = 5;
 
-            positions = simple_xy_coordinates(rg.pairtable);
-
+            // calculate the position of each nucleotide
+            // the positions of the labels will be calculated in
+            // the addLabels function
+            var positions = simple_xy_coordinates(rg.pairtable);
             rg.addPositions('nucleotide', positions)
             .addLabels(startNucleotideNumber, labelInterval);
 
-            // find out leftmost, rightmost, topmost, bottommost positions of each
-            // nucleotide so that we can create a scale
-            var xExtent = d3.extent(rg.nodes.map(function(d) { return d.x; })) 
-            var yExtent = d3.extent(rg.nodes.map(function(d) { return d.y; })) 
+            // create a transform that will fit the molecule to the
+            // size of the viewport (canvas, svg, whatever)
+            var fillViewportTransform = createTransformToFillViewport(
+                rg.nodes.map(function(d) { return d.x; }),
+                rg.nodes.map(function(d) { return d.y; }));
 
-            // add the radius of the nucleotides
-            xExtent[0] -= origRadius;
-            yExtent[0] -= origRadius;
+            var gTransform = d3.select(this)
+            .append('g')
+            .attr('transform', fillViewportTransform);
 
-            xExtent[1] += origRadius;
-            yExtent[1] += origRadius;
-
-            // find out how wide and heigh the molecule
-            var xRange = xExtent[1] - xExtent[0];
-            var yRange = yExtent[1] - yExtent[0];
-
-            // how much wider / taller is it than the available viewport
-            var xExtra = xRange - width;
-            var yExtra = yRange - height;
-
-            var xScale, yScale;
-
-            // once we have a scale for one dimension, we can create the scale for the other
-            // keeping the same expansion / shrinking ratio
-            function createOtherScale(firstScale, newDomain, newRange) {
-                var scaleFactor = (firstScale.range()[1] - firstScale.range()[0]) / 
-                                  (firstScale.domain()[1] - firstScale.domain()[0]);
-                var newWidth = (newDomain[1] - newDomain[0]) * scaleFactor
-                var newMargin = ((newRange[1] - newRange[0]) - newWidth) / 2;
-
-                return {"scaleFactor": scaleFactor, 
-                        "scale": d3.scale.linear()
-                                         .domain(newDomain)
-                                         .range([newRange[0] + newMargin, newRange[1] - newMargin])};
-            }
-
-            var ret;
-
-            if (xExtra > yExtra) {
-                // we have to shrink more in the x-dimension than the y
-                xScale = d3.scale.linear()
-                .domain(xExtent)
-                .range([0, width])
-
-                ret = createOtherScale(xScale, yExtent, [0, height]);
-                yScale = ret.scale;
-            } else {
-                // we have to shrink more in the x-dimension than the y
-                yScale = d3.scale.linear()
-                .domain(yExtent)
-                .range([0, height])
-
-                ret = createOtherScale(yScale, xExtent, [0, width]);
-                xScale = ret.scale;
-            }
-
-            //scale the size of the individual nucleotides
-            var newRadius = origRadius * ret.scaleFactor;
-            var gs = d3.select(this)
-            .selectAll('.rnaBase')
-            .data(rg.nodes)
-            .enter()
-            .append('svg:g')
-            .attr('transform', function(d) { 
-                return 'translate(' + xScale(d.x) + ',' + yScale(d.y) + ')'; 
+            var nucleotideNodes = rg.nodes.filter(function(d) { 
+                return d.nodeType == 'nucleotide'; 
+            });
+            var labelNodes = rg.nodes.filter(function(d) {
+                return d.nodeType == 'label';
             });
 
-            var circles = gs.append('svg:circle')
-            .attr('r', newRadius)
-            .classed('rnaBase', true)
+            createNucleotides(gTransform, nucleotideNodes);            
+            createLabels(gTransform, labelNodes);
 
-            addNucleotideNames(gs, rg);
         });
     }
 
